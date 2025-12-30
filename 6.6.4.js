f25de4c63d084ae8d798763b1f59fa24471d1f6c
Andrey <107883314+Kotokogotka@users.noreply.github.com>
2025-12-30 13:22:30 +0300

    Add files via upload

---
+++ 6.6.4.js
@@ -0,0 +1,11 @@
+const field = [
+    ['x', 'o', 'x'],
+    ['o', 'x', 'o'],
+    ['x', 'o', 'x'],
+];
+
+let result = '';
+
+for (let i = 0; i < field.length; i++) {
+    console.log(field[i].join(' '));
+}
